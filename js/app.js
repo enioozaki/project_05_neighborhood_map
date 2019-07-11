c2c807a97803a4948f6a9323df9f2453058a3014
Enio Ozaki <enioozaki@uol.com.br>
2019-07-11 00:17:19 -0300

    fix_css_menu_190711

--- js/app.js
+++ js/app.js
@@ -104,6 +104,18 @@ var FavoritePlace = function(data) {
   */
 }
 
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
+menu.addEventListener('click', function(e) {
+  drawer.classList.toggle('open');
+  e.stopPropagation();
+});
+main.addEventListener('click', function() {
+  drawer.classList.remove('open');
+});
+
+
 var Category = function(data) {
   this.category = ko.observable(data.category);
 }
